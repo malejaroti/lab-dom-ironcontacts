964aecb2943babd7b82ea79677c8c28f013fdb22
Alejandra Rodriguez Tibana <30965660+malejaroti@users.noreply.github.com>
2025-07-30 14:34:18 +0200

    Lab finished

--- src/index.js
+++ src/index.js
@@ -40,6 +40,37 @@ const threeContacts = contacts.splice(0, 3);
 
 // Your code goes here ...
 
+threeContacts.forEach(contact => {
+  const row = document.createElement("tr");
+  row.innerHTML = `
+  <td>
+    <img src="${contact.pictureUrl}" />
+  </td>
+  <td> ${contact.name} </td>
+  <td> ${contact.popularity.toFixed(2)} </td>
+  <td>
+    <button class="btn-delete">Delete</button>
+  </td>
+  <td>
+    <button class="btn-like">
+      <img src="./images/icon.png" alt="like" />
+    </button>
+  </td>
+  `;
+  const deleteButton = row.querySelector(".btn-delete")
+  deleteButton.addEventListener("click", () =>{
+    row.remove()
+  })
+
+  const likeButton = row.querySelector(".btn-like")
+  likeButton.addEventListener("click", () =>{
+    likeButton.classList.toggle("selected")
+  })
+
+  tableBody.appendChild(row);
+
+
+})
 
   
   // ITERATION 2 - Delete Buttons
@@ -59,3 +90,38 @@ const threeContacts = contacts.splice(0, 3);
 // Bonus: ITERATION 4 - Add Random Contacts
 
 // Your code goes here ...
+const randomIndex2 = Math.floor(Math.random() * contacts.length);
+const splicedArr2 = contacts.splice(randomIndex, 1);
+
+// Get the element from the spliced array
+const randomContact2 = splicedArr[0];
+
+const randomRow = document.createElement("tr");
+randomRow.innerHTML = `
+  <td>
+    <img src="${randomContact2.pictureUrl}" />
+  </td>
+  <td> ${randomContact2.name} </td>
+  <td> ${randomContact2.popularity.toFixed(2)} </td>
+  <td>
+    <button class="btn-delete">Delete</button>
+  </td>
+  <td>
+    <button class="btn-like">
+      <img src="./images/icon.png" alt="like" />
+    </button>
+  </td>
+`;
+  const deleteButton = randomRow.querySelector(".btn-delete")
+  deleteButton.addEventListener("click", () =>{
+    randomRow.remove()
+  })
+
+  const likeButton = randomRow.querySelector(".btn-like")
+  likeButton.addEventListener("click", () =>{
+    likeButton.classList.toggle("selected")
+  })
+
+buttonAddRandom.addEventListener("click", () => {
+  tableBody.appendChild(randomRow);
+})
